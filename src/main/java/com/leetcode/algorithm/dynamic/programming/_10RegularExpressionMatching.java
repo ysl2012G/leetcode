81b1d6c0d2d0a8c44f65023bd5cc7b8b0b6d4ced
Shuanglin <shuanglin.yu339@gmail.com>
2019-11-18 21:52:37 +0800

    leetcode 09-10

---
+++ src/main/java/com/leetcode/algorithm/dynamic/programming/_10RegularExpressionMatching.java
@@ -0,0 +1,28 @@
+package com.leetcode.algorithm.dynamic.programming;
+
+public class _10RegularExpressionMatching {
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return  false;
+        }
+
+        int sLen = s.length();
+        int pLen = p.length();
+        boolean[][] demo = new boolean[sLen + 1][pLen + 1];
+        demo[0][0] = true;
+        for (int i = 0; i <= sLen; i++){
+            for (int j = 1; j <= pLen; j++) {
+                if (j > 1 && p.charAt(j - 1) == '*') {
+                    demo[i][j] = demo[i][j -2] || (i > 0 && demo[i - 1][j ]&&(s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.'));
+                } else {
+                    demo[i][j] = i > 0 && demo[i - 1][j - 1] && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.');
+                 }
+            }
+        }
+
+        return demo[sLen][pLen];
+    }
+
+
+
+}
---
+++ src/main/java/com/leetcode/algorithm/math/_ParlindromeNumber.java
@@ -0,0 +1,18 @@
+package com.leetcode.algorithm.math;
+
+public class _ParlindromeNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x > 0 && x % 10 == 0)) {
+            return false;
+        }
+
+        int revertNum = 0;
+        while (x > revertNum) {
+            revertNum = revertNum * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return x == revertNum || x == revertNum / 10;
+    }
+
+}
